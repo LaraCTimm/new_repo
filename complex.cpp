7b01ce62faa3d28a33ce89e6ef7f29f04c53742e
LaraCTimm <l_timm@hotmail.com>
2017-08-02 16:51:26 +0200

    Exercise 3.2

--- complex.cpp
+++ complex.cpp
@@ -6,10 +6,12 @@
 
 using namespace std;
 
+using cmplxF = complex<float>;
+
 int main()
 {
-    complex<float> num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
-    complex<float> num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+    cmplxF num1{ 2.0, 2.0 };  // using C++11 uniform initialisation syntax
+    cmplxF num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
 
     auto answer = num1 * num2; // using C++11 auto keyword,
                                // answer is of type: complex<float>
@@ -17,8 +19,8 @@ int main()
     cout << "The answer is: " << answer << endl;
     cout << "Or in a more familiar form: " << answer.real() << " + " << answer.imag() << "j" << endl;
 
-    answer += 1;
-    cout << "The answer is: " << answer << endl;
+    //answer += 1;
+    //cout << "The answer is: " << answer << endl;
 
     // answer++;
     
