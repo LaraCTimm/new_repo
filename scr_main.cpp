2a1269f7607b729e315cd644b3f0e051e78bfe77
LaraCTimm <l_timm@hotmail.com>
2017-08-06 16:13:03 +0200

    Exercise 4.5

--- scr_main.cpp
+++ scr_main.cpp
@@ -1,15 +1,68 @@
 // Exercising the Screen class
+
+// Comments are inclued in the screen.cpp file as well as in this file. 
+
 #include "screen.h"
 #include <iostream>
 
 using namespace std;
 
-    enum Direction: int {HOME, FORWARD, BACK, UP, DOWN, END};
+Screen myScreen;
+enum Direction: int {HOME, FORWARD, BACK, UP, DOWN, END};
+
+void drawSquare( string::size_type startRow, string::size_type startCol, int sideLength )
+{
+    // Check for errors
+    // Check if square size is bigger than screen size
+    if (sideLength > myScreen.width() || sideLength > myScreen.height())
+    {
+        cerr << "Your square is too big to fit on the screen";
+        return;
+    }
+    if (startCol+sideLength-1 > myScreen.width() || startRow+sideLength-1 > myScreen.height())
+    {
+        cerr << "Your square is too big to fit on the screen without wrap-around. The image may not appear as intended \n";
+    }
+    
+    // set the cursor at the start position
+    myScreen.move(startRow,startCol);
+    myScreen.set('*');
+    
+    // draw top side
+    for (int i=0; i<sideLength-1; i++)
+    {
+        myScreen.move(Screen::Direction::FORWARD);
+        myScreen.set('*');
+    }
+    // draw right side
+    for (int i=0; i<sideLength-1; i++)
+    {
+        myScreen.move(Screen::Direction::DOWN);
+        myScreen.set('*');
+    }
+    // draw bottom side
+    for (int i=0; i<sideLength-1; i++)
+    {
+        myScreen.move(Screen::Direction::BACK);
+        myScreen.set('*');
+    }
+    // draw left side
+    for (int i=0; i<sideLength-1; i++)
+    {
+        myScreen.move(Screen::Direction::UP);
+        myScreen.set('*');
+    }
+    
+    myScreen.display();
+
+    return;
+}
+
 
 
 int main()
 {
-	Screen myScreen{6,6};
+	myScreen = Screen{6,6};
 
 //	myScreen.forward();
 //	myScreen.set('*');
@@ -44,7 +97,7 @@ int main()
     myScreen.set('*');
     myScreen.down();
     myScreen.set("******");
-    //myScreen.display();
+    myScreen.display();
     
     // Overload Move function
     myScreen.move(Screen::Direction::HOME);
@@ -52,7 +105,7 @@ int main()
     myScreen.move(Screen::Direction::DOWN);
     myScreen.move(Screen::Direction::BACK);
     myScreen.set("*");
-    //myScreen.display();
+    myScreen.display();
     
     // Include UP and DOWN Wrap-around
     myScreen.move(Screen::Direction::HOME);
@@ -67,8 +120,19 @@ int main()
     myScreen.set("--");
     myScreen.display();
     
-
-
+    // Draw a square on the screen.
+    myScreen.clear(' ');
+    drawSquare(3,3,6);
+    
+    /* It is not necessary to access the internal representation of the Screen class
+     * in order to implement this function. The existing interface is sufficient as
+     * all the required functionas are pubicly accessible. 
+     * 
+     * The screen object is responsible for initialising the screen and drawing a 
+     * single character at the cursor position. Drawing a square makes use of this 
+     * but is not a undamental screen function so it should not be included in 
+     * screen.cpp. 
+     * */
 
 	return 0;
 }
